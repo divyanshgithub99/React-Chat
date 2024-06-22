28a50136acc6d272125228d90fbe1d9c242374fe
Divyansh <divmatter15@gmail.com>
2024-06-22 19:31:21 +0530

    Fix multiple message id for same conversation and add logout functionality

--- client/src/modules/Dashboard/index.js
+++ client/src/modules/Dashboard/index.js
@@ -1,97 +1,115 @@
 import { useEffect, useRef, useState } from 'react'
 import { io } from 'socket.io-client'
+import { useNavigate } from 'react-router-dom';
 
 const Dashboard = () => {
-	const [user, setUser] = useState(JSON.parse(localStorage.getItem('user:detail')))
-	const [conversations, setConversations] = useState([])
-	const [messages, setMessages] = useState({})
-	const [message, setMessage] = useState('')
-	const [users, setUsers] = useState([])
-	const [socket, setSocket] = useState(null)
-	const messageRef = useRef(null)
+  const [user, setUser] = useState(JSON.parse(localStorage.getItem('user:detail')))
+  const [conversations, setConversations] = useState([])
+  const [messages, setMessages] = useState({ messages: [] })
+  const [message, setMessage] = useState('')
+  const [users, setUsers] = useState([])
+  const [socket, setSocket] = useState(null)
+  const [selectedUserId, setSelectedUserId] = useState(null)
+  const messageRef = useRef(null)
 
-	useEffect(() => {
-		setSocket(io('http://localhost:8000'))
-	}, [])
+  const navigate = useNavigate();
 
-	useEffect(() => {
-		socket?.emit('addUser', user?.id);
-		socket?.on('getUsers', users => {
-			console.log('activeUsers :>> ', users);
-		})
-		socket?.on('getMessage', data => {
-			setMessages(prev => ({
-				...prev,
-				messages: [...prev.messages, { user: data.user, message: data.message }]
-			}))
-		})
-	}, [socket])
 
-	useEffect(() => {
-		messageRef?.current?.scrollIntoView({ behavior: 'smooth' })
-	}, [messages?.messages])
+  useEffect(() => {
+    setSocket(io('http://localhost:8000'))
+  }, [])
 
-	useEffect(() => {
-		const loggedInUser = JSON.parse(localStorage.getItem('user:detail'))
-		const fetchConversations = async () => {
-			const res = await fetch(`http://localhost:8080/api/conversations/${loggedInUser?.id}`, {
-				method: 'GET',
-				headers: {
-					'Content-Type': 'application/json',
-				}
-			});
-			const resData = await res.json()
-			setConversations(resData)
-		}
-		fetchConversations()
-	}, [])
+  useEffect(() => {
+    if (socket) {
+      socket.emit('addUser', user?.id);
+      socket.on('getUsers', users => {
+        console.log('activeUsers :>> ', users);
+      })
+      socket.on('getMessage', data => {
+        setMessages(prev => ({
+          ...prev,
+          messages: [...(prev.messages || []), { user: data.user, message: data.message }]
+        }))
+      })
+    }
+  }, [socket])
 
-	useEffect(() => {
-		const fetchUsers = async () => {
-			const res = await fetch(`http://localhost:8080/api/users/${user?.id}`, {
-				method: 'GET',
-				headers: {
-					'Content-Type': 'application/json',
-				}
-			});
-			const resData = await res.json()
-			setUsers(resData)
-		}
-		fetchUsers()
-	}, [])
+  useEffect(() => {
+    messageRef?.current?.scrollIntoView({ behavior: 'smooth' })
+  }, [messages?.messages])
 
-	const fetchMessages = async (conversationId, receiver) => {
-		const res = await fetch(`http://localhost:8080/api/message/${conversationId}?senderId=${user?.id}&&receiverId=${receiver?.receiverId}`, {
-			method: 'GET',
-			headers: {
-				'Content-Type': 'application/json',
-			}
-		});
-		const resData = await res.json()
-		setMessages({ messages: resData, receiver, conversationId })
-	}
+  useEffect(() => {
+    const loggedInUser = JSON.parse(localStorage.getItem('user:detail'))
+    const fetchConversations = async () => {
+      const res = await fetch(`http://localhost:8080/api/conversations/${loggedInUser?.id}`, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        }
+      });
+      const resData = await res.json()
+      setConversations(resData)
+    }
+    fetchConversations()
+  }, [])
 
-	const sendMessage = async (e) => {
-		setMessage('')
-		socket?.emit('sendMessage', {
-			senderId: user?.id,
-			receiverId: messages?.receiver?.receiverId,
-			message,
-			conversationId: messages?.conversationId
-		});
-		const res = await fetch(`http://localhost:8080/api/message`, {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json',
-			},
-			body: JSON.stringify({
-				conversationId: messages?.conversationId,
-				senderId: user?.id,
-				message,
-				receiverId: messages?.receiver?.receiverId
-			})
-		});
-	}
+  useEffect(() => {
+    const fetchUsers = async () => {
+      const res = await fetch(`http://localhost:8080/api/users/${user?.id}`, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        }
+      });
+      const resData = await res.json()
+      setUsers(resData)
+    }
+    fetchUsers()
+  }, [])
+
+  const fetchMessages = async (conversationId, receiver) => {
+    if (selectedUserId !== receiver?.receiverId) {
+      const res = await fetch(`http://localhost:8080/api/message/${conversationId}?senderId=${user?.id}&&receiverId=${receiver?.receiverId}`, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        }
+      });
+      const resData = await res.json()
+      setMessages({ messages: resData, receiver, conversationId })
+      setSelectedUserId(receiver?.receiverId)
+    }
+  }
+
+  const sendMessage = async (e) => {
+    setMessage('')
+    socket?.emit('sendMessage', {
+      senderId: user?.id,
+      receiverId: messages?.receiver?.receiverId,
+      message,
+      conversationId: messages?.conversationId
+    });
+    const res = await fetch(`http://localhost:8080/api/message`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        conversationId: messages?.conversationId,
+        senderId: user?.id,
+        message,
+        receiverId: messages?.receiver?.receiverId
+      })
+    });
+  }
+
+  const handleLogout = async () => {
+    console.log("div...")
+    await localStorage.removeItem('user:detail')
+    await localStorage.removeItem('user:token')
+    navigate('/users/sign_in')
+    alert('Logout Successfully!')
+  }
 
 return (
   <div className='flex'>
@@ -103,6 +121,7 @@ return (
         <div className='ml-4'>
 						<h3 className='text-2xl'>{user?.fullName}</h3>
 						<p className='text-lg font-light'>My Account</p>
+            <button onClick={handleLogout} className='mt-2 p-2 bg-red-500 text-white rounded-lg'>Logout</button>
         </div>
       </div>
       <hr className="border-gray-600" />
